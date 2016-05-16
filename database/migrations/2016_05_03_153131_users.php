7a5d32d7d5848943134d82599f6890d5c4635713
Pablo Laris <pablo@silverip.com>
2016-05-16 11:14:16 -0500

    tablesUp to date

--- database/migrations/2016_05_03_153131_users.php
+++ database/migrations/2016_05_03_153131_users.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class User extends Migration
+class Users extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class User extends Migration
      */
     public function up()
     {
-      Schema::create('user', function(Blueprint $table)
+      Schema::create('users', function(Blueprint $table)
       {
         $table->increments('id');
         $table->string('first_name')->comment("-First Name");
@@ -36,6 +36,6 @@ class User extends Migration
      */
     public function down()
     {
-      Schema::drop('user');
+      Schema::drop('users');
     }
 }
