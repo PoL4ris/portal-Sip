afc0621058d4e353de54954705ee4ef416e9bbe0
Peyman Pourkermani <peyman@silverip.com>
2016-07-16 00:30:46 -0500

    Fixed create_tickets_table migration: midiumText => mediumText

--- database/migrations/2016_05_18_165656_create_tickets_table.php
+++ database/migrations/2016_05_18_165656_create_tickets_table.php
@@ -19,7 +19,7 @@ class CreateTicketsTable extends Migration
         $table->string('ticket_number')->comment('Ticket Number');
         $table->text('vendor_ticket')->nullable()->comment('Vendor ID');
         $table->integer('id_reasons')->comment('Reason');
-        $table->midiumText('comment')->nullable()->comment('Comment');
+        $table->mediumText('comment')->nullable()->comment('Comment');
         $table->string('status');
         $table->integer('id_users')->comment('Created By');
         $table->integer('id_users_assigned')->nullable()->comment('Assigned to');
