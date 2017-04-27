9495886332f510ced44617ab6fb4840e5eebfecb
Peyman Pourkermani <peyman@silverip.com>
2017-04-27 12:58:09 -0500

    Added new migration for a temp column to the customers table

---
+++ database/migrations/2017_04_27_115645_add_password_updated_column_to_customers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPasswordUpdatedColumnToCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->integer('password_updated')->after('password')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn('password_updated');
+        });
+    }
+}
