b690bc42239555f60d64814228911804c55d5054
Peyman Pourkermani <peyman@silverip.com>
2017-08-17 14:05:50 -0500

    Added manager mass email notification to GeneralTask.

---
+++ resources/views/email/template_manager_notification.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+    <title></title>
+    <meta name='Generator' content='Cocoa HTML Writer'>
+    <meta name='CocoaVersion' content='1038.35'>
+    <style type='text/css'>
+        p.p1 {margin: 0px 0px 0px 0px; font: 14px Avenir; }
+        p.p2 {margin: 0px 0px 0px 0px; font: 14px Avenir; min-height: 14px}
+        p.p3 {margin: 0px 0px 0px 0px; font: 14px Avenir; }
+        p.p4 {margin: 0px 0px 0px 0px; font: 14px Avenir; min-height: 14px}
+        span.s1 {font: 14.0px Avenir}
+    </style>
+</head>
+<body>
+{{--<p class='p1'><strong><img src="http://www.silverip.com/silverip-tiny-logo-clear.png" alt=" SilverIP Communications"--}}
+                           {{--width="165" height="70"></strong><br>--}}
+<p class='p2'></p>
+<p class='p1'>Good Afternoon,</p>
+<p class='p2'></p>
+<p class='p1'>On Monday, we experienced a rare network complication that caused an outage in portions of our network.  The outage was due to equipment failure of a core device at one of our data centers.  Unfortunately, the backup system did not activate correctly during this incident.  As soon as we were alerted of the problem, our network operations team was immediately dispatched and all efforts went towards resolving this matter as quickly as possible.  We certainly apologize for any inconvenience this may have caused.</p>
+<p class='p2'></p>
+<p class='p1'>Needless to say, we take this matter very seriously.  In light of this, we will be scheduling a <strong>maintenance window during off-peak hours</strong> to replace the faulty core device and test our backup procedures.</p>
+<p class='p2'></p>
+<p class='p1'><strong>Date:  Tuesday, August 22</strong></p>
+<p class='p1'><strong>Time:  1am - 5am</strong></p>
+<p class='p2'></p>
+<p class='p1'>During this window of 1am - 5am, you may experience intermittent service disruptions.  We greatly appreciate your continued support and we thank you for your understanding.</p>
+<p class='p4'></p>
+<p class='p3'>Sincerely,</p>
+<p class='p1'>
+    <font face="Avenir, serif">
+        <font color="#063dcc"><font style="font-size:9pt" size="2"><b>_______________________________________</b></font></font><br/>
+        <font color="#0b5ab2"><font style="font-size:11pt" size="2">
+                <i><b>Jon Nierstheimer</b></i></font></font><br/>
+        <font color="#343434"><font style="font-size:10pt" size="2">
+                <i>Director of Operations</i></font></font><br/>
+        <img src="http://www.silverip.com/silverip-tiny-logo-clear.png" alt=" SilverIP Communications"
+             width="165" height="70"><br/>
+        <font color="#063dcc"><font style="font-size:9pt" size="2"><b>_______________________________________</b></font></font><br/>
+    </font>
+</p>
+<p class='p2'></p>
+</body>
+</html>
